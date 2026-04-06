ef86a7c30761339f5ba270fb90cdd109f87e3b99
francescopassante <francescopassante@icloud.com>
2026-04-06 22:00:43 +0200

    added data/ to .gitignore and built plot function for neighbors of a point in Preprocessing

--- GET/Preprocessing.py
+++ GET/Preprocessing.py
@@ -1,3 +1,4 @@
+import numpy as np
 import potpourri3d as pp3d
 import trimesh
 
@@ -15,20 +16,56 @@ class MeshPreprocessor:
             mesh.apply_scale(1 / area)
         return mesh
 
-    solver = pp3d.MeshHeatMethodDistanceSolver(self.mesh.vertices, self.mesh.faces)
+    def compute_geodesic_neighborhood(self):
 
-    # 3. Compute distances from a source vertex p (index p_idx)
-    p_idx = 0
-    distances = solver.compute_distance(p_idx)
+        solver = pp3d.MeshHeatMethodDistanceSolver(self.mesh.vertices, self.mesh.faces)
 
-    # 4. Select vertices within the 0.2 geodesic radius
-    neighborhood_mask = distances <= 0.2
-    neighbor_indices = np.where(neighborhood_mask)[0]
+        # 3. Compute distances from a source vertex p (index p_idx)
+        p_idx = 5000
+        distances = solver.compute_distance(p_idx)
 
-    print(f"Vertex {p_idx} has {len(neighbor_indices)} neighbors within radius 0.2")
+        # 4. Select vertices within the 0.2 geodesic radius
+        neighbor_indices = np.where(distances <= 0.1)[0]
+        return neighbor_indices
+
+    # Function to plot the neighbors of vertex 0:
+    def plot_neighbors(self, neighbor_indices):
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
+        ax.scatter(
+            self.mesh.vertices[:, 0],
+            self.mesh.vertices[:, 1],
+            self.mesh.vertices[:, 2],
+            color="lightgray",
+            s=10,
+        )
+        ax.scatter(
+            self.mesh.vertices[neighbor_indices, 0],
+            self.mesh.vertices[neighbor_indices, 1],
+            self.mesh.vertices[neighbor_indices, 2],
+            color="red",
+            s=10,
+        )
+
+        # Plot the source vertex in blue
+        ax.scatter(
+            self.mesh.vertices[5000, 0],
+            self.mesh.vertices[5000, 1],
+            self.mesh.vertices[5000, 2],
+            color="blue",
+            s=50,
+        )
+        ax.set_title("Geodesic Neighborhood of Vertex 0")
+        plt.show()
 
 
 if __name__ == "__main__":
     # Example usage
-    path = "data/T0.off"  # Replace with your mesh file path
-    preprocessor = MeshPreprocessor(path).mesh.show()
+    path = "data/SHREC11_test_database_new/T42.off"  # Replace with your mesh file path
+    preprocessor = MeshPreprocessor(path)
+    neighbours = preprocessor.compute_geodesic_neighborhood()
+    # Use function to plot the neighbors of vertex 0:
+    preprocessor.plot_neighbors(neighbours)
