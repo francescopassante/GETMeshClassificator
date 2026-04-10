95784470bd18a1e00339c607f2ccba67c79130e9
francescopassante <francescopassante@icloud.com>
2026-04-10 17:29:09 +0200

    Reorganized GEBlocks equivariance test in check_equivariance()

--- GET/src/GEBlocks.py
+++ GET/src/GEBlocks.py
@@ -52,30 +52,30 @@ class LinearEquivariant(nn.Module):
 
 if __name__ == "__main__":
     # I want to check that if i rotate an input (x,y,z) then apply the layer i get a permutation of the output fields:
+    def check_equivariance(x, theta):
+        def rotate_input(x, theta):
+            # Rotate around the z-axis by angle theta
+            cos_theta = torch.cos(theta)
+            sin_theta = torch.sin(theta)
+            rotation_matrix = torch.tensor(
+                [[cos_theta, -sin_theta, 0], [sin_theta, cos_theta, 0], [0, 0, 1]],
+                dtype=torch.float32,
+            )
+            return x @ rotation_matrix.t()
 
-    def rotate_input(x, theta):
-        # Rotate around the z-axis by angle theta
-        cos_theta = torch.cos(theta)
-        sin_theta = torch.sin(theta)
-        rotation_matrix = torch.tensor(
-            [[cos_theta, -sin_theta, 0], [sin_theta, cos_theta, 0], [0, 0, 1]],
-            dtype=torch.float32,
-        )
-        return x @ rotation_matrix.t()
+        utils = GEUtils.Linear(N=9)
+        W_basis = utils.compute_W_basis()  # This should give us a list of numpy arrays
+        W_basis_torch = [torch.tensor(W, dtype=torch.float32) for W in W_basis]
+        equivariant_layer = LinearEquivariant(W_basis=W_basis_torch, num_fields=12)
 
-    utils = GEUtils.Linear(N=9)
-    W_basis = utils.compute_W_basis()  # This should give us a list of numpy arrays
-    W_basis_torch = [torch.tensor(W, dtype=torch.float32) for W in W_basis]
-    equivariant_layer = LinearEquivariant(W_basis=W_basis_torch, num_fields=12)
+        # Rotate the input by 2pi/9 (the angle corresponding to the cyclic group C9) and check the output
+        theta = torch.tensor(2 * 8 * np.pi / 9, dtype=torch.float32)
 
-    # Rotate the input by 2pi/9 (the angle corresponding to the cyclic group C9) and check the output
-    theta = torch.tensor(2 * 8 * np.pi / 9, dtype=torch.float32)
+        input = torch.randn(1, 1, 3)  # Original input
+        rotated_input = rotate_input(input, theta)
 
-    input = torch.randn(1, 1, 3)  # Original input
-    rotated_input = rotate_input(input, theta)
+        output = equivariant_layer(input).view(1, 1, 12, 9)
+        rotated_output = equivariant_layer(rotated_input).view(1, 1, 12, 9)
 
-    output = equivariant_layer(input).view(1, 1, 12, 9)
-    rotated_output = equivariant_layer(rotated_input).view(1, 1, 12, 9)
-
-    print(output[0][0][3])  # Should be (1, 1, 12, 9)
-    print(rotated_output[0][0][3])  # Should be (1, 1, 12, 9)
+        print(output[0][0][3])  # Should be (1, 1, 12, 9)
+        print(rotated_output[0][0][3])  # Should be (1, 1, 12, 9)
